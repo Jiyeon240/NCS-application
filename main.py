146d2068e981183d7c655167701b3c7c77b38e86
jiyeon240 <hongsidjr@naver.com>
2026-03-17 17:54:33 +0900

    week 03 pandas dataframe 2

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import pandas as pd
 
+#items = {
+#    'a' : [100,80,90],
+#    'b' : [95,75,85],
+#    'c' : [70,100,99]
+#}
+
 items = {
-    'a' : [100,80,90],
-    'b' : [95,75,85],
-    'c' : [70,100,99]
+    [100,80,90]
+    [95,75,85]
+    [70,100,99]
 }
 
-df_items = pd.DataFrame(items, index = [1,2,3])
+df_items = pd.DataFrame(items, index = [1,2,3], columns=['a','b','c'])
 print(df_items)
 
