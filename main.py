728001bfbf1557dfa544c2845da844f47afee25b
jiyeon240 <hongsidjr@naver.com>
2026-03-17 17:52:18 +0900

    week 03 pandas dataframe 1

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-import numpy as np
+import pandas as pd
 
-items = [40,7,99,-3]
+items = {
+    'a' : [100,80,90],
+    'b' : [95,75,85],
+    'c' : [70,100,99]
+}
 
-scores = np.array([
-    [80,100,90]
-    [80,75,95]
-    [80,99,87]
-])
+df_items = pd.DataFrame(items, index = [1,2,3])
+print(df_items)
 
-print(f"전체 평균 : {np.means(scores)}")
-print(f"국 영 수 과목별 평균 : {np.mean(scores, axis=0)}")
-#axis =0 열별 평균
-
-print(f"a학생, b학생, c학생 최고점수 : {np.max(scores, axis=1)}")
